848d611037aba1c13cdf6a6abdf5e2a33c27b2be
SSRam1919 <saisriram1919@gmail.com>
2023-07-22 18:08:46 +0530

    alphabet

---
+++ alphabet.py
@@ -0,0 +1,19 @@
+import string
+
+alph = list(string.ascii_uppercase)
+
+pan = []
+count = 0
+for i in alph:
+    temi = ""
+    temi += i
+    for j in alph:
+        temj = ""
+        temj = temi+j
+        for k in alph:
+            temk = ""
+            temk = temj+k
+            pan.append(temk)
+            count += 1
+
+print(count)
